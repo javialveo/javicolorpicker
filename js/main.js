feb7d2e8784935bdd712b6def216c7d1c62e0cae
Eduardo Alveo <development@javialveo.com>
2022-04-08 20:43:18 -0500

    Primera versión que permite mostrar el color seleccionado por medio de selector HTML, y los despliega el color, junto al código Hexadecimal.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-function main(){
-  const VERSION_APP = "0.1.5";
-  
-  const displayVersion = document.querySelector("#versionApp");
-  
-  displayVersion.textContent = VERSION_APP;
+"use strict";
+
+const APP_VERSION = "0.0.6";
+const label_version = document.querySelector("#versionApp");
+
+const colorBox = document.querySelector("#colorBox");
+const htmlSelector = document.querySelector("#htmlSelector");
+
+const hexadecimalColor = document.querySelector("#hexaColor");
+const rgbColor = document.querySelector("#rgbColor");
+
+
+function setHexadecimalCode(color){
+  hexadecimalColor.textContent = color;
 }
 
+function setBoxColor(color){
+  colorBox.setAttribute("style", `background-color: ${color}`);
+}
+
+
+function main(){
+  label_version.textContent = APP_VERSION;
+}
+
+
 main();
+
+htmlSelector.addEventListener("input", () => {
+  setBoxColor(htmlSelector.value);
+  setHexadecimalCode(htmlSelector.value);
+})
