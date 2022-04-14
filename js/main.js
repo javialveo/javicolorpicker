9ae5eaebade1e35e63a37800c22601dcf3f09c34
Eduardo Alveo <development@javialveo.com>
2022-04-14 00:37:01 -0500

    Se renueva la hoja de estilos y la maquetación de la web.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function setAppVersion() {
-  const APP_VERSION = "0.0.10";
+  const APP_VERSION = "1.0.0";
   const label_version = document.querySelector("#versionApp");
 
   label_version.textContent = APP_VERSION;
@@ -10,56 +10,56 @@ function setAppVersion() {
 function setDefaultValue() {
   const DEFAULT_COLOR = "#f1f1f1";
 
-  const colorOutput = document.querySelector("#colorBox");
+  const colorOutput = document.querySelector("#colorOutput");
   const htmlSelector = document.querySelector("#htmlSelector");
-  const redRange = document.querySelector("#redRange");
-  const greenRange = document.querySelector("#greenRange");
-  const blueRange = document.querySelector("#blueRange");
+  const redLevel = document.querySelector("#redLevel");
+  const greenLevel = document.querySelector("#greenLevel");
+  const blueLevel = document.querySelector("#blueLevel");
 
-  const redLabel = document.querySelector("#redLabel");
-  const greenLabel = document.querySelector("#greenLabel");
-  const blueLabel = document.querySelector("#blueLabel");
+  const amountOfRed = document.querySelector("#amountOfRed");
+  const amountOfGreen = document.querySelector("#amountOfGreen");
+  const amountOfBlue = document.querySelector("#amountOfBlue");
 
-  const hexadecimalCode = document.querySelector("#hexaColor");
-  const RGBCode = document.querySelector("#rgbColor");
+  const hexadecimalCode = document.querySelector("#hexaCode");
+  const rgbCode = document.querySelector("#rgbCode");
 
   colorOutput.style.backgroundColor = DEFAULT_COLOR;
   htmlSelector.value = DEFAULT_COLOR;
-  redRange.value = 241;
-  greenRange.value = 241;
-  blueRange.value = 241;
+  redLevel.value = 241;
+  greenLevel.value = 241;
+  blueLevel.value = 241;
 
-  redLabel.textContent = redRange.value;
-  greenLabel.textContent = greenRange.value;
-  blueLabel.textContent = blueRange.value;
+  amountOfRed.textContent = redLevel.value;
+  amountOfGreen.textContent = greenLevel.value;
+  amountOfBlue.textContent = blueLevel.value;
 
   hexadecimalCode.textContent = DEFAULT_COLOR;
-  RGBCode.textContent = colorOutput.style.backgroundColor;
+  rgbCode.textContent = colorOutput.style.backgroundColor;
 
 
   console.log(colorOutput.style.backgroundColor);
 }
 
 function getColorBox() {
-  const colorOutput = document.querySelector("#colorBox");
+  const colorOutput = document.querySelector("#colorOutput");
 
   return colorOutput.style.backgroundColor;
 }
 
 function setColorOutput(color) {
-  const colorOutput = document.querySelector("#colorBox");
+  const colorOutput = document.querySelector("#colorOutput");
 
   colorOutput.style.backgroundColor = color;
 }
 
 function setHexadecimalCode(color) {
-  const hexadecimalCode = document.querySelector("#hexaColor");
+  const hexadecimalCode = document.querySelector("#hexaCode");
 
   hexadecimalCode.textContent = color;
 }
 
 function setRGBCode(color) {
-  const rgbCode = document.querySelector("#rgbColor");
+  const rgbCode = document.querySelector("#rgbCode");
 
   rgbCode.textContent = color;
 }
